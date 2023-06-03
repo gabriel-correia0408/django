53f524988a006cc7d6c51dda39d285a87733d5d6
Gabriel Correia <gabrielcorreia0913@gmail.com>
2023-06-02 21:18:44 -0300

    aula 13 modulo tres curso django, usando shell do django

--- core/views.py
+++ core/views.py
@@ -4,9 +4,21 @@ from django.shortcuts import render
 
 
 def index(request):
+    print(dir(request.user))
+    print(f'Metodo:{request.method}')
+    print(f"User: {request.user}")
+    print(f"GETHOST: {request.get_host}")
+    print(f"ISSECURE: {request.is_secure}")
+
+    if str(request.user) == 'AnonymousUser':
+        teste = 'Usuario nao logado'
+    else:
+        teste = 'Usuario  logado'
+
     context = {
         'curso': 'Programação web com Django Framework',
-        'outraChave': 'Django é massa'
+        'outraChave': 'Django é massa',
+        'logado': teste
     }
     return render(request, 'index.html', context)
 
